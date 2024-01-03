e6b5501f23dfa589e47051239051d90ab8510b5c
misnosugianto48 <misnosugianto48@gmail.com>
2024-01-03 15:54:33 +0700

    make model, fake, seeder, controller transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['staff_id', 'total_amount'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Staff::class);
+    }
+
+    public function voucher()
+    {
+        return $this->hasOne(Voucher::class);
+    }
+}
---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Staff;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'staff_id' => Staff::all()->random()->id,
+            'total_amount' => fake()->numberBetween(1000, 2000000),
+        ];
+    }
+}
